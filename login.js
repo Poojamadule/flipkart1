74edc7070d13a467a773c7d1baaa091d210c9094
Poojamadule <poojamadule25@gmail.com>
2022-12-01 12:55:49 +0530

    login completed

--- login.js
+++ login.js
@@ -1,2 +1,11 @@
-reg_email="abcde";
-password="12345";
+let reg_email="abcde";
+let reg_password="12345";
+
+let enter_email="abcde";
+let enter_password="12345";
+if(reg_email==enter_email && reg_password==enter_password){
+console.log("login successfull")
+}
+else{
+console.log("login failed")
+}
